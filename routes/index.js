873f2d66a459425067d06d001d4b5a49cd6fad84
HKThienYM <khanhthien60@gmail.com>
2016-11-28 20:54:36 +0700

    update index

--- routes/index.js
+++ routes/index.js
@@ -90,18 +90,6 @@ global.blog = [
         title:'EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT',
         date:'November 10, 2015',
         content:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    },
-    {
-        img:'images/bui5.jpg',
-        title:'EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT',
-        date:'November 10, 2015',
-        content:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    },
-    {
-        img:'images/fo3.jpg',
-        title:'EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT',
-        date:'November 10, 2015',
-        content:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
 ];
 
